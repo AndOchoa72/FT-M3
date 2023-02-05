3bb686b9afae4edc2a8608322b60ce39810bbcdc
Andres Ochoa <andochoa@yahoo.com.ar>
2023-02-05 17:04:37 -0300

    Hw01 terminada

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,10 +1,31 @@
+const { stdout, stdin } = require("process");
 const process = require("process");
 const commands = require("./commands/index.js");
 
 function bash() {
+  stdout.write("prompt > ");
+  stdin.on("data", function (data) {
+    args = data.toString().trim().split(" ");
+    cmd = args.shift();
+//    console.log("*Args:", args);
+//    console.log("*Cmd:", cmd);
+    if (commands.hasOwnProperty(cmd)) {
+        commands[cmd](print, args.join(" "));
+      } else {
+        print ("command not found: " + cmd);
+      }
+  });
+}
+    
+function print(output) {
+  stdout.write(output);
+  stdout.write("\nprompt > ");
 }
 
+// console.log(commands);
+
 bash();
+
 module.exports = {
   print,
   bash,
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,20 +2,114 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+//    console.log("Llegamos al print");
+    print(process.cwd());
+}
 
-function date() {}
+function date(print) {
+//    console.log("Llegamos a la funcion Date");
+    print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+//    console.log("Llegamos a la funcion Echo");
+    print(args);
+}
 
-function ls() {}
+function ls(print) {
+//    console.log("Llegamos a la funcion ls");
+    fs.readdir(".", (err, files) => {
+        if (err) {
+//            console.log(err);
+            throw Error(err);
+        };
+//          console.log(files);
+        rds = files.join(" ");
+//           console.log(rds);
+        print(rds);
+    });
+}
 
-function cat() {}
+function cat(print, args) {
+//    console.log("Llegamos a la funcion cat");
+//    console.log(args);
+//    args = args.slice(1).join(" ");
+    if (args == "") {
+        print("No files specified.");
+    } else {
+//        console.log(args);
+        fs.readFile(args, "utf-8",(err, lines) => {
+            if (err) throw Error(err);
+            lines = lines.toString();
+//            console.log(lines);
+            print(lines);
+        });
+    }
+}
 
-function head() {}
+function head(print, args) {
+//    console.log("Llegamos a la funcion head");
+//    console.log(args);
+//    args = args.slice(1).join(" ");
+    if (args == "") {
+        print("No files specified.");
+    } else {
+//        console.log(args);
+        fs.readFile(args, "utf-8",(err, lines) => {
+            if (err) throw Error(err);
+//            console.log("* " + lines);
+            lines = lines + "\n";
+            lines = lines.substring(0, lines.indexOf("\n"));
+            print(lines);
+        });
+    }
+}
 
-function tail() {}
+function tail(print, args) {
+//    console.log("Llegamos a la funcion tail");
+//    console.log("1:" + args);
+//    args = args.slice(1).join(" ");
+    if (args == "") {
+        print("No files specified.");
+    } else {
+        console.log(args);
+        fs.readFile(args, "utf-8",(err, lines) => {
+            if (err) throw Error(err);
+//            console.log("*:" + lines);
+//            lines = "\n" + lines;
+//            lines = lines.substring(lines.lastIndexOf("\n"));
+//            print(lines);
+            print(lines.split(" ").at(-1));
+        });
+    }
+}
 
-function curl() {}
+function curl(print, args) {
+//    console.log("Llegamos a la funcion curl");
+//    console.log(args);
+//    args = args.slice(1).join(" ");
+    if (args == "") {
+        print("No WebPages specified.");
+    } else {
+//        console.log(args);
+        utils.request(args,(err, response) => {
+        if (err) throw Error(err);
+//        console.log(response.data);
+        // lines = "\n" + lines;
+        // lines = lines.substring(lines.lastIndexOf("\n"));
+        print(response);
+        });
+    }
+}
 
-module.exports = {};
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
+};
