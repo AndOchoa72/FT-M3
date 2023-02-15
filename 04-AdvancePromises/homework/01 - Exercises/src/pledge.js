ed69aa6fdfa04486c2c09bd9d1678b1e398564e5
AndOchoa72 <andochoa@yahoo.com.ar>
2023-02-15 11:32:43 -0300

    hw04 - 02

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -8,11 +8,12 @@ const _TSPending = 'pending';
 const _TSFullFilled = 'fulfilled';
 const _TSRejected = 'rejected';
 
-function $Promise (executor) { // Declarada por Function
-    this._state = _TSPending;
+function $Promise (executor) {
     if (typeof executor !== 'function') {
         throw new TypeError('The executor must be function type.');
     };
+    this._state = _TSPending;
+	this._handlerGroups = [];
     executor(
         this._internalResolve.bind(this),
         this._internalReject.bind(this)
@@ -23,13 +24,37 @@ $Promise.prototype._internalResolve = function(data) {
     if (this._state === _TSPending) {
         this._state = _TSFullFilled;
         this._value = data;
+		this._callHandlers();
     };
 };
 $Promise.prototype._internalReject = function(reason) {
     if (this._state === _TSPending) {
         this._state = _TSRejected;
         this._value = reason;
-    };
+		this._callHandlers();
+    }; 
+};
+$Promise.prototype.then = function(successCb, errorCb) {
+ 	if (typeof successCb !== "function") successCb = false;
+	if (typeof errorCb !== "function") errorCb = false;
+	this._handlerGroups.push({
+					successCb,
+					errorCb
+					});
+	if (this._state != _TSPending) { this._callHandlers()};
+};
+$Promise.prototype._callHandlers = function() {
+	while (this._handlerGroups.length > 0) {
+		let handlersActuales = this._handlerGroups.shift();
+		let successCb = handlersActuales.successCb;
+		let errorCb = handlersActuales.errorCb;
+		if (this._state == _TSFullFilled) {
+			if (successCb !== false) successCb(this._value);
+		};
+		if (this._state == _TSRejected) {
+			if (errorCb !== false) errorCb(this._value);
+		};		
+	}; 
 };
    
 module.exports = $Promise;
