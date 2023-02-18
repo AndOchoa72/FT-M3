0d41d1a1a635f9f4ee33f0ad4bacc3c6072f3411
Andres Ochoa <andochoa@yahoo.com.ar>
2023-02-18 00:02:43 -0300

    hw05 - 02

--- 05-Express/homework/01 - Exercises/src/server.js
+++ 05-Express/homework/01 - Exercises/src/server.js
@@ -3,27 +3,49 @@ const express = require("express");
 let publications = [];
 
 const server = express();
-const JsonErr = {error: "No se recibieron los parámetros necesarios para crear la publicación"};
 
 server.use(express.json());
 
+const JsonErr1 = {error: "No se recibieron los parámetros necesarios para crear la publicación"};
+const JsonErr2 = {error: "No existe ninguna publicación con dicho título y autor indicado"};
+let id = 1;
+
 server.post('/posts', function (req, res) {
-	var obj = req.body;
-//	console.log(req);
-	if (   (!obj.hasOwnProperty('author'))
-		|| (!obj.hasOwnProperty('title'))
-		|| (!obj.hasOwnProperty('contents'))
-		) {	res.status(400);
-			return res.end( JSON.stringify(JsonErr));
-			};
-	miObj = {
-		id: obj.id,
-		author: obj.author,
-		title: obj.title,
-		contents: obj.contents
+	const p = req.body;
+//	console.log(p);
+	if (p.author && p.title && p.contents) {
+//		console.log('p.ok.');
+		const publication = {
+			author: p.author,
+			title: p.title,
+			contents: p.contents,
+			id: id++
+		}
+//		console.log(publication);
+		publications.push(publication);
+		return res.status(200).json(publication);
+	} else {
+//		console.log('p.mal.');
+		return res.status(400).json(JsonErr1);
 		};
-	publications.push(miObj);
-	res.send(JSON.stringify(miObj));
+});
+
+server.get('/posts', function (req, res) {
+
+	const {reqAuthor, reqTitle} = req.query;
+	if(reqAuthor && reqTitle) {
+		filtPub = publications.filter(
+			pub => pub.author === reqAuthor
+				&& pub.title === reqTitle);
+		if (filtPub.length) {
+			res.status(200);
+			return res.json(filtPub);
+		} else {
+			return res.status(400).json(JsonErr2);
+		}
+ 	} else {
+ 		return res.status(400).json(JsonErr2);
+ 	}
 });
 
 //NO MODIFICAR EL CODIGO DE ABAJO. SE USA PARA EXPORTAR EL SERVIDOR Y CORRER LOS TESTS
